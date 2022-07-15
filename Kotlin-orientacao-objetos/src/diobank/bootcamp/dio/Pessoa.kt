7af7a550ff26126a93d0395a67f2c19c45007b7c
Wanuzia Braga <wanuzia.braga@grupoboticario.com.br>
2022-07-15 19:28:50 -0300

    update to apply the private modifier to set function of cpf property

--- Kotlin-orientacao-objetos/src/diobank/bootcamp/dio/Pessoa.kt
+++ Kotlin-orientacao-objetos/src/diobank/bootcamp/dio/Pessoa.kt
@@ -3,11 +3,11 @@ package diobank.bootcamp.dio
 class Pessoa {
     var nome: String = "Maria"
     var cpf: String = "001.005.556-65"
+    private set
 }
 
 fun main() {
     val maria = Pessoa()
     println(maria.nome)
     println(maria.cpf)
-    println("Hello world")
 }
