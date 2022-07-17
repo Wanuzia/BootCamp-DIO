7b920af3c920546261271c280acc81e1628fe016
Wanuzia Braga <wanuzia.braga@grupoboticario.com.br>
2022-07-16 23:52:41 -0300

    create interArray test

---
+++ Kotlin-collections/src/main/dio/collections/TesteIntArrays.kt
@@ -0,0 +1,27 @@
+package dio.collections
+
+fun main() {
+    val values = IntArray(5)
+        values[0] = 3
+        values[1] = 18
+        values[2] = 5
+        values[3] = 7
+        values[4] = 10
+
+    for(valor in values) {
+        println(valor)
+    }
+    println("-------------")
+    values.forEach {
+        println(it)
+    }
+    println("-------------")
+    for (index in values.indices){
+        println(values[index])
+    }
+    println("-------------")
+    values.sort() 
+    for(valor in values) {
+        println(valor)
+    }
+}
