e2d3aa09a7d1e26d1b10c8a4469f8e38fbbffc0c
Wanuzia Braga <wanuzia.braga@grupoboticario.com.br>
2022-07-17 01:44:12 -0300

    created collections operations

--- Kotlin-collections/src/main/dio/collections/TesteOperacoes.kt
+++ Kotlin-collections/src/main/dio/collections/TesteOperacoes.kt
@@ -15,6 +15,16 @@ fun main() {
 
     val salarioMaiorQue2500 = salarios.filter { it > 2500.0 }
     salarioMaiorQue2500.forEach{println(it)}
+
     println("---------------outra forma de imprimir:")
     println("Salários acima de 2.500,00 : $salarioMaiorQue2500")
+
+    println("---------------Conta quantos elementos estão dentro dos critérios:")
+    println(salarios.count { it in 2000.0..5000.0 })
+
+    println("---------------buscar um valor/elemento específico:")
+    println(salarios.find { it == 2800.0})
+
+    println("---------------retorna boolean")
+    println(salarios.any { it == 2800.0})
 }
