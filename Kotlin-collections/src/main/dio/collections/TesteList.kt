d60bcaa08f2181298cefbcf110ad565263078a8b
Wanuzia Braga <wanuzia.braga@grupoboticario.com.br>
2022-07-17 02:13:46 -0300

    created list tests

---
+++ Kotlin-collections/src/main/dio/collections/TesteList.kt
@@ -0,0 +1,25 @@
+package dio.collections
+
+fun main() {
+    val maria = Funcionario(nome = "Maria", salario = 2800.0)
+    val dandara = Funcionario(nome = "Dandara", salario = 5000.0)
+    val alice = Funcionario(nome = "Alice", salario = 1500.0)
+
+    val funcionarios = listOf(maria, dandara, alice)
+
+    funcionarios.forEach{ println(it) }
+    
+    println("---------------")
+    println(funcionarios.find { it.nome == "Maria" })
+}
+data class Funcionario(
+    val nome: String,
+    val salario: Double
+){
+    override fun toString(): String =
+        """
+        Nome: $nome
+        Salário: $salario
+        """.trimIndent()
+    }
+
